af0e05e35a2a5acad8e619bf34910d28daab75ed
Daria Golovashova <darja.golovashova@gmail.com>
2026-04-10 21:40:16 +0200

    Initial commit

---
+++ app/notes/page.tsx
@@ -0,0 +1,20 @@
+import { dehydrate, QueryClient } from "@tanstack/react-query";
+import { fetchNotes } from "@/lib/api";
+import NotesClient from "./Notes.client";
+import { HydrationBoundary } from "@tanstack/react-query";
+
+async function NotesPage() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["notes"],
+    queryFn: () => fetchNotes(),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient />
+    </HydrationBoundary>
+  );
+}
+export default NotesPage;
