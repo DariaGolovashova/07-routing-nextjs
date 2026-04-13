2fa5b9e6435c44580406ece126b16949a37ce178
Daria Golovashova <darja.golovashova@gmail.com>
2026-04-13 11:58:39 +0200

    last

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -8,8 +8,8 @@ function NotesLayout({
   return (
     <div style={{ display: "flex", gap: "20px" }}>
       <aside>{sidebar}</aside>
-      {/* <div style={{ flex: 1 }}>{children}</div> */}
-      <main>{children}</main>
+      <div style={{ flex: 1 }}>{children}</div>
+      {/* <main>{children}</main> */}
     </div>
   );
 }
